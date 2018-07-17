a630b6b982b69f6d4363f38eed7ba796075aaa46
purushothamanbalakrishnan <purushothamanbalakrishnan@10.0.0.73>
2018-07-17 16:19:40 -0700

    codes are committed

---
+++ JDBCDB/src/main.java
@@ -0,0 +1,60 @@
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class main
+ */
+@WebServlet("/main")
+public class main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public main() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3036/emply","root", "rootpuru");
+			Statement stmt=conn.createStatement();
+			ResultSet rs=stmt.executeQuery("select*from rover");
+			while(rs.next())
+			{
+				System.out.println(rs.getString(1)+ "" +rs.getInt(1)+ " " +rs.getString(2) +" "+rs.getInt(2));
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
